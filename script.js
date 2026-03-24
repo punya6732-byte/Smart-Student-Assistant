2ce4b4add900f72361ad36863608cf40038ed3e8
Punya <sandhyachamaraju@gmail.com>
2026-03-24 23:52:31 +0530

    Final working script

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 function addtask(){
     let task =document.getElementById("task").value;
+    if (task ===" ") return;
     let li=document.createElement("li");
     li.textContent=task;
-
+    
     document.getElementById("taskList").appendChild(li);
+    savetask(task);
+    document.getElementById("task").value=" ";
+}
+function saveTask(task){
+    let tasks=JSON.parse(localStorage.getItem("tasks"))||[];
+    tasks.push(task);
+    localStorage.setItem("tasks",JSON.stringify(tasks));
 }
 let words=["안녕하세요 (Hello)", "감사합니다 (Thank you)", "사랑해요 (I love you)", "공부 (Study)"];
 function newWord(){
@@ -23,3 +31,28 @@ function newQuote(){
     let random=Math.floor(Math.random()*quotes.length);
     document.getElementById("quote").textContent=quotes[random];
 }
+
+function calculateGPA(){
+    let marks=document.getElementById("marks").value;
+    if (marks ===" ")return;
+    let gpa =(marks/10).toFixed(2);
+document.getElementById("gpa").textContent="GPA:" +gpa;
+}
+
+function updateStreak(){
+    let streak=localStorage.getItem("streak")||0;
+    streak++;
+    localStorage.setItem("streak").textContent=streak +" days";
+
+}
+
+window.onlosd=function(){
+    let tasks=JSON.parse(localStorage.getItem("tasks"))||[];
+    tasks.forEach(task=>{
+        let li=document.createElement("li");
+        li.textContent=task;
+        document.getElementById("taskList").appendChild(li);
+
+    });
+    let streak=localStorage.getItem("streak").textContent=streak +" days";
+};
